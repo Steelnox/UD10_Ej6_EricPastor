0b76ab399331f80d7e26e288f825ab18dfa4aaa0
Steelnox <eric14pastor@gmail.com>
2021-01-09 22:31:14 +0100

    Ejercicio 6 acabado

---
+++ UD10_Ej6/UD10_Ej6/Libro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10_Ej6
+{
+    public class Libro
+    {
+
+        private string isbn;
+        private string titulo;
+        private string autor;
+        private int npaginas;
+
+        public Libro(string isbn, string titulo, string autor, int npaginas)
+        {
+            this.isbn = isbn;
+            this.titulo = titulo;
+            this.autor = autor;
+            this.npaginas = npaginas;
+        }
+
+        public Libro()
+        {
+            isbn = "";
+            titulo = "";
+            autor = "";
+            npaginas = 0;
+        }
+
+        public string Isbn { get => isbn; set => isbn = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public int Npaginas { get => npaginas; set => npaginas = value; }
+
+        public override string ToString()
+        {
+            return "El libro " + titulo.ToString() + " con ISBN " + isbn.ToString() + " creado por el autor " + autor.ToString() + " tiene " + npaginas.ToString() + " paginas";
+        }
+    }
+}
---
+++ UD10_Ej6/UD10_Ej6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UD10_Ej6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro1 = new Libro("45879123G", "El principe maldito", "Johnson Stuart", 500);
+            Libro libro2 = new Libro("87962233W", "El periplo del rey maldito", "Michael Steward", 850);
+
+            Console.WriteLine(libro1.ToString());
+            Console.WriteLine(libro2.ToString());
+
+            if (libro1.Npaginas > libro2.Npaginas)
+            {
+                Console.WriteLine("El libro {0} tiene más paginas que el libro {1}", libro1.Titulo, libro2.Titulo);
+            }
+            else Console.WriteLine("El libro {0} tiene más paginas que el libro {1}", libro2.Titulo, libro1.Titulo);
+        }
+    }
+}
